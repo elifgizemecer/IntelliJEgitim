e911640e6b32c7d553d4392b7dd5154b2b69cd8e
elifgizemecer <elifgizemecer@gmail.com>
2022-05-12 12:28:30 +0300

    Initial Commit

---
+++ src/main/java/tr/com/testinium/Edu1.java
@@ -0,0 +1,8 @@
+package tr.com.testinium;
+
+public class Edu1 {
+    public static void main(String[] args){
+        System.out.println("Selam");
+        //terminalde git add . yazdık ve edu yeşil oldu
+    }
+}
